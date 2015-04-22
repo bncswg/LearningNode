ed31e436769221feb943c8dd46ecd1e8da659197
Ben Wong <ben@simplepickup.com>
2015-04-22 13:38:06 -0700

    Lesson 3: File Read Stream

---
+++ FileReadStream.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+var file = fs.createReadStream("fruits.txt");
+
+file.on('readable',function() {
+
+  var chunk = null;
+  
+  while (null !== (chunk = file.read())) {
+    console.log(chunk.toString());
+  }
+});
